e1a3402c6e1219ca0ec4f21a05d7dff16c122f86
Alicia Francis <aliciafrancis@Alicias-MBP.attlocal.net>
2023-09-21 16:12:20 -0500

    commit 2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Product } from './components/Product';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Reward Points Calculator</h1>
+        <Product />
+      </header>
+    </div>
+  );
+}
+
+export default App;
